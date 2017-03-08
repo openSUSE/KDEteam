df3061a962730b9f958592dbf2a4dba383e4b471
Fabian Vogt <fabian@ritter-vogt.de>
2017-03-08 19:14:50 +0100

    Plasma: Improve mkchanges.sh
    - Iterate all commits instead of huge piping
    - Output kde# bugrefs

--- Plasma/mkchanges.sh
+++ Plasma/mkchanges.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 if [[ $# -eq 1 ]]; then
     echo "-------------------------------------------------------------------"
     LANG=C date | tr -d '\n'
@@ -19,6 +19,32 @@ version_from=$3
 version_to=$4
 type=$5
 
+function entryForCommit {
+    commit="$1"
+    subject="$(git show -s --pretty=format:%s "$commit")"
+
+    # Ignore GIT_SILENT/SVN_SILENT
+    if [[ "${subject/_SILENT}" != "${subject}" ]]; then
+        return
+    fi
+
+    echo -n "  * ${subject}"
+
+    # Get bugrefs
+    bugs="$(git show -s "$commit" | grep -E '^\s*BUG:' | cut --delimiter=: --fields=2 | sed 's/^/,kde#/')"
+
+    if [[ -n "${bugs}" ]]; then
+        echo -n " ("
+        # Remove first ','
+        for i in ${bugs#,}; do
+            echo -n "${i}"
+        done
+        echo -n ")"
+    fi
+
+    echo "\n"
+}
+
 echo "-------------------------------------------------------------------"
 LANG=C date | tr -d '\n'
 echo -e " - fabian@ritter-vogt.de\n"
@@ -26,10 +52,18 @@ echo "- Update to $version_to"
 echo "  * New $type release"
 echo "  * For more details please see:"
 echo "  * https://www.kde.org/announcements/plasma-$4.php"
-echo "- Changes since $version_from:"
-changes=$(git log --pretty=oneline --no-merges $commit_from...$commit_to | cut -d' ' -f2- | grep -v "_SILENT" | sed "s/^/  * /")
-if [ -z "$changes" ]; then
-    changes="  * None"
+commits=$(git log --pretty=format:%H --no-merges $commit_from...$commit_to)
+if [ "$(echo -- "$commits" | wc -l)" -gt 30 ]; then
+    changes="- Too many changes to list here"
+else
+    for i in $commits; do
+        changes="${changes}$(entryForCommit "$i")"
+    done
+
+    echo "- Changes since $version_from:"
+    if [ -z "$changes" ]; then
+        changes="  * None"
+    fi
 fi
-echo "$changes"
+echo -e "$changes"
 echo ""
