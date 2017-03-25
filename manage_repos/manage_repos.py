26027f33386bb6f9d8382a4589edbb0091810eae
Luca Beltrame <lbeltrame@kde.org>
2017-03-25 22:52:59 +0100

    Very non-atomic change
    This commit unfortunately changes code all around because I had to
    adjust many parts, The net effect is that now updating from an unstable
    repository (like, from KUA to KA) is now supported, via a separate
    command.
    
    This added more complexity in one function that had enough parameters, I
    will think about a way to make it simpler...

--- manage_repos/manage_repos.py
+++ manage_repos/manage_repos.py
@@ -33,6 +33,25 @@ CHANGES_TEMPLATE = """
 CHANGES_ENTRY = "  * {subject} {bugs}"
 
 
+def _check_path(path: str) -> Path:
+
+    if path is None:
+        return
+
+    path = Path(path).expanduser().absolute()
+    return path
+
+
+def _add_patch_information(contents: list, patches: list, text: str=None):
+
+    if not patches or not contents:
+        return
+
+    contents.append(text)
+    for patch in patches:
+        contents.append(CHANGES_ENTRY.format(subject=patch, bugs=""))
+
+
 @contextmanager
 def cd(subpath):
     old_path = Path.cwd()
@@ -56,9 +75,11 @@ def format_log_entries(commit_from: str, commit_to: str) -> str:
 
     if not all_commits:
         yield "  * None"
+        return
 
     if len(all_commits) > 30:
         yield "  * Too many changes to list here"
+        return
 
     for commit in all_commits:
 
@@ -101,7 +122,9 @@ def create_dummy_changes_entry(version_to: str, destination: str,
 
 def create_changes_entry(repo_name: str, commit_from: str, commit_to: str,
                          version_from: str, version_to: str, changetype: str,
-                         kind: str, destination: str, committer: str) -> None:
+                         kind: str, destination: str, committer: str,
+                         previous_patches: list=None,
+                         current_patches: list=None) -> None:
 
     contents = list()
     contents.append("- Update to {}".format(version_to))
@@ -114,8 +137,38 @@ def create_changes_entry(repo_name: str, commit_from: str, commit_to: str,
 
     contents.append("- Changes since {}:".format(version_from))
 
+    commit_data = list()
+
     for entry in format_log_entries(commit_from, commit_to):
-        contents.append(entry)
+        commit_data.append(entry)
+
+    if not commit_data:
+        contents.append("  * None")
+    else:
+        contents.extend(commit_data)
+
+    if current_patches and previous_patches:
+        patches_difference = set(current_patches) - set(previous_patches)
+    else:
+        patches_difference = set()
+
+    if patches_difference:
+
+        added_patches = list()
+        removed_patches = list()
+
+        print("Patches changed.")
+        print("The program will record these changes, but this will require"
+              " MANUAL REVIEW. Their nature cannot be guessed automatically.")
+
+        for patch in patches_difference:
+            if patch in current_patches:
+                added_patches.append(patch)
+            elif patch in previous_patches:
+                removed_patches.append(patch)
+
+        _add_patch_information(contents, added_patches, "- Added patches:")
+        _add_patch_information(contents, added_patches, "- Removed patches:")
 
     contents = "\n".join(contents)
     date = time.strftime("%a %b %d %H:%M:%S %Z %Y")
@@ -133,7 +186,8 @@ def create_changes_entry(repo_name: str, commit_from: str, commit_to: str,
 def record_changes(changes_file: str, checkout_dir: Path, version_from: str,
                    version_to: str, *, upstream_reponame: str,
                    changetype: str="bugfix", kind: str="applications",
-                   committer: str =None, branch: str=None) -> None:
+                   committer: str=None, branch: str=None,
+                   **kwargs) -> None:
 
     package_name = changes_file.replace(".changes", "")
     commit_from = "v{}".format(version_from)
@@ -165,7 +219,7 @@ def record_changes(changes_file: str, checkout_dir: Path, version_from: str,
 
         create_changes_entry(upstream_reponame, commit_from, commit_to,
                              version_from, version_to, changetype, kind,
-                             changes_file, committer)
+                             changes_file, committer, **kwargs)
 
 
 def upstream_tag_available(tag: str) -> bool:
@@ -179,7 +233,7 @@ def upstream_tag_available(tag: str) -> bool:
 # Spec file handling
 
 
-def parse_spec(specfile: Path) -> (str, list):
+def parse_spec(specfile: Path) -> (str, list, str):
 
     specfile = Spec.from_file(str(specfile))
     version = specfile.version
@@ -196,12 +250,12 @@ def parse_spec(specfile: Path) -> (str, list):
     # FIXME: Won't work for special oS macros
     upstream_source = replace_macros(upstream_source, specfile)
 
-    patches = None if not hasattr(specfile, "patches") else specfile.patches
+    patches = list() if not hasattr(specfile, "patches") else specfile.patches
 
     return version, patches, upstream_source
 
 
-def update_version(specfile: str, version_to: str, patches=None) -> None:
+def update_version(specfile: str, version_to: str) -> None:
 
     with fileinput.input(specfile, inplace=True) as f:
         for line in f:
@@ -213,9 +267,10 @@ def update_version(specfile: str, version_to: str, patches=None) -> None:
 
 
 def update_package(entry: Path, version_to: str, tarball_directory: Path=None,
-                   *, committer: str, kind: str="applications",
-                   changetype: str="bugfix", checkout_dir: Path=None,
-                   upstream_branch: str=None) -> bool:
+                   *, version_from: str=None, committer: str=None,
+                   kind: str="applications", changetype: str="bugfix",
+                   checkout_dir: Path=None, upstream_branch: str=None,
+                   previous_patches: list=None) -> bool:
 
     package_name = entry.name
     specfile = package_name + ".spec"
@@ -225,9 +280,15 @@ def update_package(entry: Path, version_to: str, tarball_directory: Path=None,
 
     current_version, patches, upstream_reponame = parse_spec(specfile)
 
+    # This happens when copying packages from a different repository,
+    # the version may be behind, so if it's explicitly specified,
+    # we override the one in the spec
+
+    current_version = current_version if version_from is None else version_from
+
     if current_version == version_to:
-        print("Package {} is already at the latest version {}. Skipping.".format(
-            package_name, current_version))
+        print("Package {} is already at the latest version {}"
+              ". Skipping.".format(package_name, current_version))
         return False
 
     print("Updating package {}".format(package_name))
@@ -235,7 +296,7 @@ def update_package(entry: Path, version_to: str, tarball_directory: Path=None,
     tarball_name = "{name}-{version_to}.tar.xz".format(name=upstream_reponame,
                                                        version_to=version_to)
 
-    update_version(specfile, version_to, patches)
+    update_version(specfile, version_to)
 
     if tarball_directory is not None and tarball_directory.exists():
         done_subdir = tarball_directory / "done"
@@ -268,7 +329,9 @@ def update_package(entry: Path, version_to: str, tarball_directory: Path=None,
 
     record_changes(changes_file, checkout_dir, current_version,
                    version_to, upstream_reponame=upstream_reponame,
-                   changetype=changetype, kind=kind, committer=committer)
+                   changetype=changetype, kind=kind, committer=committer,
+                   previous_patches=previous_patches,
+                   current_patches=patches)
 
     if Path("pre_checkin.sh").exists():
         run("pre_checkin.sh", shell=True)
@@ -283,24 +346,130 @@ def make_changes(parser, context, args):
 
     parser.add_argument("--version-from", help="New version to update from")
     parser.add_argument("--version-to", help="New version to update to")
-    parser.add_argument("-s", "--checkout-dir",
-                        help="Directory containing source checkouts")
     parser.add_argument("-t", "--type", choices=("bugfix", "feature"),
                         help="Type of release (bugfix or feature)",
                         default="bugfix")
-    parser.add_argument("-e", "--committer", required=True,
-                        help="Email address of the committer")
     parser.add_argument("changes_file", help="Changes file to update")
     parser.add_argument("upstream_name", help="Upstream package name")
 
     options = parser.parse_args(args)
     # We "cd" inside other directories, so make the path to outside ones
     # absolute
-    checkout_dir = Path(options.checkout_dir).expanduser().absolute()
+
+    checkout_dir = _check_path(context.checkout_dir)
+
     record_changes(options.changes_file, checkout_dir,
                    options.version_from, options.version_to,
                    upstream_reponame=options.upstream_name,
-                   committer=options.committer, branch=None)
+                   committer=context.committer, branch=None)
+
+
+@subcmd
+def sync_from_unstable_project(parser, context, args):
+
+    parser.add_argument("--tarball-dir",
+                        help="Directory containing source tarballs")
+    parser.add_argument("-k", "--kind", default="applications",
+                        choices=("plasma", "frameworks", "applications",
+                                 "other"))
+    parser.add_argument("-b", "--stable-branch",
+                        help="Use information from this branch"
+                        " if a tag is not available")
+    parser.add_argument("--version-to", help="New version to update to")
+    parser.add_argument("source",
+                        help=("Directory containing checkouts"
+                              " from the OBS unstable project"))
+    parser.add_argument("destination",
+                        help="Directory containing checkouts from the OBS"
+                        " development project to be updated")
+
+    update_type = "feature"
+
+    options = parser.parse_args(args)
+    results = Counter()
+
+    source_dir = _check_path(options.source)
+    tarball_directory = _check_path(options.tarball_dir)
+    checkout_dir = _check_path(context.checkout_dir)
+    destination_dir = _check_path(options.destination)
+
+    print("Source (unstable) dir: {}".format(source_dir))
+    print("Destination (devel) dir: {}".format(destination_dir))
+
+    for entry in source_dir.iterdir():
+
+        if not entry.is_dir() or entry.name.startswith("."):
+            continue
+
+        entry = entry.absolute()
+        package_name = entry.name + ".spec"
+        corresponding = destination_dir / entry.name
+
+        if not corresponding.exists():
+            print("Missing destination directory {}. Skipping.".format(
+                corresponding))
+            results.update(["missing"])
+            continue
+
+        destination_spec = corresponding / package_name
+
+        # Get current version and patches in use
+        version, patches, _ = parse_spec(destination_spec)
+
+        if version == options.version_to:
+            print("Package {} already updated, skipping.".format(
+                corresponding))
+            results.update(["failedskipped"])
+            continue
+
+        # Remove everything locally
+
+        for item in corresponding.iterdir():
+            if item.name.startswith(".") or item.name.endswith(".changes"):
+                continue
+            item.unlink()
+
+        print("Copying {} from Unstable project to development".format(
+            entry.name))
+
+        # Copy over all the files
+
+        for item in entry.iterdir():
+
+            if (item.name.startswith(".") or item.name == "_service" or
+                    item.name.endswith(".changes")):
+                continue
+
+            destination_file = corresponding / item.name
+            shutil.copy(str(item), str(destination_file))
+
+        with cd(corresponding):
+
+            result = update_package(corresponding, options.version_to,
+                                    tarball_directory,
+                                    committer=context.committer,
+                                    kind=options.kind, changetype=update_type,
+                                    checkout_dir=checkout_dir,
+                                    upstream_branch=options.stable_branch,
+                                    previous_patches=patches,
+                                    version_from=version)
+            if result:
+                results.update(["updated"])
+
+                # Copy the updated .changes file to the unstable project
+                new_changes_file = corresponding / (corresponding.name +
+                                                    ".changes")
+                old_changes_file = entry / (entry.name + ".changes")
+
+                shutil.copy(str(new_changes_file), str(old_changes_file))
+            else:
+                results.update(["failedskipped"])
+
+        results.update(["total"])
+
+    print("Processed {} packages: updated {}, failed/skipped {},"
+          " missing {}".format(results["total"], results["updated"],
+                               results["failedskipped"], results["missing"]))
 
 
 @subcmd
@@ -314,30 +483,18 @@ def update_packages(parser, context, args):
         help="Use information from this branch if a tag is not available")
     parser.add_argument("--tarball-dir",
                         help="Directory containing source tarballs")
-    parser.add_argument("-s", "--checkout-dir",
-                        help="Directory containing source checkouts")
     parser.add_argument("--version-to", help="New version to update to")
     parser.add_argument("-k", "--kind", default="applications",
                         choices=("plasma", "frameworks", "applications",
                                  "other"))
-    parser.add_argument("-e", "--committer", required=True,
-                        help="Email address of the committer")
     parser.add_argument("directory", help="Directory with the OBS checkout")
 
     options = parser.parse_args(args)
 
     results = Counter()
-    # We "cd" inside other directories, so make the path to outside ones
-    # absolute
-    if options.tarball_dir is not None:
-        tarball_directory = Path(options.tarball_dir).expanduser().absolute()
-    else:
-        tarball_directory = None
 
-    if options.checkout_dir is not None:
-        checkout_dir = Path(options.checkout_dir).expanduser().absolute()
-    else:
-        checkout_dir = None
+    tarball_directory = _check_path(options.tarball_dir)
+    checkout_dir = _check_path(options.checkout_dir)
 
     for entry in Path(options.directory).iterdir():
 
@@ -347,9 +504,10 @@ def update_packages(parser, context, args):
         entry = entry.absolute()
 
         with cd(entry):
+
             result = update_package(entry, options.version_to,
                                     tarball_directory,
-                                    committer=options.committer,
+                                    committer=context.committer,
                                     kind=options.kind, changetype=options.type,
                                     checkout_dir=checkout_dir,
                                     upstream_branch=options.stable_branch)
@@ -367,6 +525,13 @@ def update_packages(parser, context, args):
 def main():
 
     handler = ArgumentHandler()
+
+    # Global parameters
+    handler.add_argument("-e", "--committer", required=True,
+                         help="Email address of the committer")
+    handler.add_argument("-s", "--checkout-dir",
+                         help="Directory containing source checkouts")
+
     handler.run()
 
 
